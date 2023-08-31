d366159c254addce644bc978cfe0a64a44e882e4
Prashanth <Prashanth@APK-HOME>
2023-08-31 12:12:29 +0530

    This is committ

---
+++ Selenium_advance/src/advance2.java
@@ -0,0 +1,16 @@
+
+public class advance {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("hiiiii");
+		
+		System.out.println("hello");
+		System.out.println("how are you");
+		System.out.println("submitted");
+		
+
+	}
+
+}
